aa97ba986a882f3e0d74a6d0986ae2d81f892fbc
Logan Elandt <elandtdevelops@gmail.com>
2021-05-14 16:54:43 -0500

    Complete chapter 2: Creational Patterns of Python: Design Patterns course

--- python_design_patterns/Exercise Files/Ch02/02_06/singleton.py
+++ python_design_patterns/Exercise Files/Ch02/02_06/singleton.py
@@ -1,12 +1,45 @@
 class Borg:
 
-    """The Borg design pattern"""
+    """ The Borg design pattern
+        Borg class making class attributes global
+    """
+
+    _shared_state = {}  # Attribute dictionary
+
+    def __init__(self) -> None:
+        self.__dict__ = self._shared_state  # Make it an attribute distionary
 
 
+class Singleton(Borg):  # Inherits from the Borg class
+    """
+        The singleton class
+        This class now shares all of its attributes among its various instances
+    """
+
+    # This essentially makes the singleton objects an object-oriented
+    # global variable
+
+    def __init__(self, **kwargs) -> None:
+        # Need to call Borg.__init__(self) and NOT super().__init__(self)
+        Borg.__init__(self)
+        # update the attribute dictionary by inserting a new key-value pair
+        self._shared_state.update(kwargs)
+
+    def __str__(self) -> str:
+        # Returns the attribute dictionary for printing
+        return str(self._shared_state)
 
 
-class Singleton(Borg):
+# Let's create a singlteton object and add our first acornym
+x = Singleton(HTTP="Hyper Test Transfer Protocol")
 
-    """The singleton class"""
+# Print the object
+print(f"x = {x}")
 
+# Let's create another singleton object, and if it refers to the same
+# attribute dictionary by adding another acronym
+y = Singleton(SNMP="Simple Network Management Protocol")
 
+# Print the object
+print(f"y = {y}")
+print(f"x = {x}")
--- python_design_patterns/Exercise Files/Ch02/02_10/prototype.py
+++ python_design_patterns/Exercise Files/Ch02/02_10/prototype.py
@@ -1,31 +1,45 @@
 import copy
 
+# Clones objects according to a prototypical instance - useful
+# when creating many identical objects individually
+# Related to the Abstract Factory pattern
+
+
 class Prototype:
-    
     def __init__(self):
-        
-        
+        self._objects = {}
+
     def register_object(self, name, obj):
         """Register an object"""
-        
-        
+        self._objects[name] = obj
+
     def unregister_object(self, name):
         """Unregister an object"""
-        
-        
+        del self._objects[name]
+
     def clone(self, name, **attr):
         """Clone a registered object and update its attributes"""
-        
-        
+        # Store the cloned object
+        obj = copy.deepcopy(self._objects.get(name))
+        # Update the clone
+        obj.__dict__.update(attr)
+        # Return the clone
+        return obj
+
+
 class Car:
     def __init__(self):
         self.name = "Skylark"
         self.color = "Red"
         self.options = "Ex"
-        
+
     def __str__(self):
-        return '{} | {} | {}'.format(self.name, self.color, self.options)
-        
+        return f"{self.name} | {self.color} | {self.options  }"
 
 
+c = Car()
+prototype = Prototype()
+prototype.register_object("skylark", c)
 
+c1 = prototype.clone("skylark")
+print(c1)
