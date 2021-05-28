e3301091515458c2e0fd3e56eb5c6672160994e2
Logan Elandt <elandtdevelops@gmail.com>
2021-05-28 16:23:41 -0500

    Complete the remaining Behavioral pattern examples.

--- python/python_design_patterns/Ch04/04_10/chain.py
+++ python/python_design_patterns/Ch04/04_10/chain.py
@@ -1,50 +1,67 @@
-class Handler: #Abstract handler
-	"""Abstract Handler"""
-	def __init__(self, successor):
-		# Define who is the next handler
+# Chain of Responsibility pattern decouples the request and its processing
+# Related to Composite
 
-	def handle(self, request):
-			handled = self._handle(request) #If handled, stop here
 
-			#Otherwise, keep going
-			if not handled:
-					
+# Abstract handler
+class Handler:
+    """Abstract Handler"""
+    def __init__(self, successor):
+        # Define who is the next handler
+        self._successor = successor
 
-	def _handle(self, request):
-		raise NotImplementedError('Must provide implementation in subclass!')
+    def handle(self, request):
+        # If handled, stop here
+        handled = self._handle(request)
 
-class ConcreteHandler1(Handler): # Inherits from the abstract handler
-	"""Concrete handler 1"""
-	def _handle(self, request):
-		if 0 < request <= 10: # Provide a condition for handling
-			print("Request {} handled in handler 1".format(request))
-			return True # Indicates that the request has been handled
+        # Otherwise, keep going
+        if not handled:
+            self._successor.handle(request)
 
-class DefaultHandler(Handler): # Inherits from the abstract handler
-	"""Default handler"""
+    def _handle(self, request):
+        raise NotImplementedError('Must provide implementation in subclass!')
 
-	def _handle(self, request):
-		"""If there is no handler available"""
-		#No condition checking since this is a default handler
-		print("End of chain, no handler for {}".format(request))
-		return True # Indicates that the request has been handled
 
-class Client: # Using handlers
-	def __init__(self):
-		# Create handlers and use them in a sequence you want
-		                                                      # Note that the default handler has no successor
+# Inherits from the abstract handler
+class ConcreteHandler1(Handler):
+    """Concrete handler 1"""
+    def _handle(self, request):
+        # Provide a condition for handling
+        if 0 < request <= 10:
+            print(f"Request {request} handled in handler 1")
+            # Indicates that the request has been handled
+            return True
 
-	def delegate(self, requests): # Send your requests one at a time for handlers to handle
-		for request in requests:
-				self.handler.handle(request)
+
+# Inherits from the abstract handler
+class DefaultHandler(Handler):
+    """Default handler"""
+
+    def _handle(self, request):
+        """If there is no handler available"""
+        # No condition checking since this is a default handler
+        print(f"End of chain, no handler for {request}")
+        # Indicates that the request has been handled
+        return True
+
+
+# Using handlers
+class Client:
+    def __init__(self):
+        # Create handlers and use them in a sequence you want
+        # Note that the default handler has no successor
+        self.handler = ConcreteHandler1(DefaultHandler(None))
+
+    # Send your requests one at a time for handlers to handle
+    def delegate(self, requests):
+        for request in requests:
+            self.handler.handle(request)
+
 
 # Create a client
-
+c = Client()
 
 # Create requests
-requests = []
+requests = [2, 5, 30]
 
 # Send the requests
-
-
-
+c.delegate(requests)
