4c6a69bddc6e99a136f99f66c157c37eb80b3d8f
Logan Elandt <elandtdevelops@gmail.com>
2021-05-28 11:34:40 -0500

    Complete observer, visitor, and iterator behavioral pattern examples.

--- python/python_design_patterns/Ch04/04_06/iterator.py
+++ python/python_design_patterns/Ch04/04_06/iterator.py
@@ -1,20 +1,30 @@
+# Iterator pattern - allows a client to have sequential access
+# to an aggregate object without exposing the underlying structure
+# Related to Composite
+
+
 def count_to(count):
-	"""Our iterator implementation"""
-	
-	#Our list
-	numbers_in_german = ["eins", "zwei", "drei", "vier", "funf"]
+    """Our iterator implementation"""
 
-	#Our built-in iterator
-	#Creates a tuple such as (1, "eins")
-	
-	
-	#Iterate through our iterable list
-	#Extract the German numbers
-	#Put them in a generator called number
-	for position, number in iterator:
-		
-		#Returns a 'generator' containing numbers in German
-		
-#Let's test the generator returned by our iterator
+    # Our list
+    numbers_in_german = ["eins", "zwei", "drei", "vier", "funf"]
 
-	
+    # Our built-in iterator
+    # Creates a tuple such as (1, "eins")
+    iterator = zip(range(count), numbers_in_german)
+
+    # Iterate through our iterable list
+    # Extract the German numbers
+    # Put them in a generator called number
+    for position, number in iterator:
+
+        # Returns a 'generator' containing numbers in German
+        yield number
+
+
+# Let's test the generator returned by our iterator
+for num in count_to(3):
+    print(num)
+
+for num in count_to(4):
+    print(num)
